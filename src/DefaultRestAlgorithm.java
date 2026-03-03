a2ee0db8c298959f760d7e177f963335fbc5c14a
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-03-03 10:49:39 +0530

    Algo

---
+++ src/DefaultRestAlgorithm.java
@@ -0,0 +1,30 @@
+public class DefaultRestAlgorithm implements RestAlgorithm {
+
+    private final long workDurationMs;
+    private final long restDurationMs;
+
+    /**
+     * Creates an algorithm with custom durations.
+     */
+    public DefaultRestAlgorithm(long workDurationMs, long restDurationMs) {
+        this.workDurationMs = workDurationMs;
+        this.restDurationMs = restDurationMs;
+    }
+
+    /**
+     * Creates an algorithm with the standard 20-minute work, 20-second rest rule.
+     */
+    public DefaultRestAlgorithm() {
+        this(20 * 60 * 1000L, 20 * 1000L); // 20 minutes work, 20 seconds rest
+    }
+
+    @Override
+    public long getWorkDurationMs() {
+        return workDurationMs;
+    }
+
+    @Override
+    public long getRestDurationMs() {
+        return restDurationMs;
+    }
+}
---
+++ src/RestAlgorithm.java
@@ -0,0 +1,15 @@
+public interface RestAlgorithm {
+    /**
+     * Gets the duration of the work period in milliseconds.
+     * 
+     * @return Work duration in milliseconds.
+     */
+    long getWorkDurationMs();
+
+    /**
+     * Gets the duration of the rest period in milliseconds.
+     * 
+     * @return Rest duration in milliseconds.
+     */
+    long getRestDurationMs();
+}
